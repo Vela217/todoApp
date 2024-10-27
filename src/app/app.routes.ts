1ba08a72eb13ba97e426df1852aac49a2f19b0f7
Nicolas Vela <nicolasvela63@gmail.com>
2024-10-27 17:01:24 -0500

    String interpolatión

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component'
+import { LabsComponent } from './pages/labs/labs.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        //path nombre de nuestra ruta
+        path: '', //Para que la pagina inical quede home o la ruta principal
+        //path: 'home',
+        component: HomeComponent //El componente que renderiza
+    },
+    {
+        path: 'labs',
+        component: LabsComponent
+    },
+
+];
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
-
+  title = 'miApp';
+  lista = ["Aprender", "Angular", "Hoy"]
+  bandera = true
+  protected name = "Juan Nicolas Vela Tovar"
+  edad = 21
 }
